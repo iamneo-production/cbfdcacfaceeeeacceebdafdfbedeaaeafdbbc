321575a4c710fb50f060bb46687f24894634e2a6
Iamneo <techadmin@iamneo.ai>
2023-08-08 08:30:54 +0000

    Backup Commit

---
+++ reactapp/src/Components\UI/Card/Card.jsx
@@ -0,0 +1,7 @@
+import '../../../index.css';
+import Button from '../Button/Button'
+import React from 'react';
+
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
